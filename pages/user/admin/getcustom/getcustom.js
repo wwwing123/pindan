2c0c5892d3d859b209ead8ab9ef0ae62f26bf576
孔瑞荣 <kongruirong@MacBook-Pro-2.local>
2021-03-22 08:36:58 +0800

    111

--- pages/user/admin/getcustom/getcustom.js
+++ pages/user/admin/getcustom/getcustom.js
@@ -79,7 +79,7 @@ Page({
     const year = date.getFullYear();
     const month = (date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : '' + (date.getMonth() + 1);
     const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
-    return `${year}.${month}.${day}`
+    return `${year}/${month}/${day}`
   },
 
   getdiffDays:function(start,end){
